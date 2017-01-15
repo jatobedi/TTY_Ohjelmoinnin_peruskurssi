e0d22dd4b8f38d97509fb9413fe550c6231f5ed4
Janne Aare <janne.aare@student.tut.fi>
2017-01-15 13:53:06 +0200

    Ennakkotehtava 01 paalooppi toimii.

--- ennakkotehtava-01/main.cpp
+++ ennakkotehtava-01/main.cpp
@@ -3,12 +3,39 @@
 
 using namespace std;
 
+void tulosta_riveja(int rivien_maara) {
+    while (rivien_maara > 0){
+        cout << endl;
+        rivien_maara --;
+    }
+}
+
 int main()
 {
-    string sana = "";
-    cout << "Syota sana: ";
-    getline(cin, sana);
+    string sana = "kellopeliappelsiini";
+    string arvattusana = "___________________";
+    const int sallitut_virheet = 5;
+    int virheet = 0;
+    string::size_type mitta = 0;
+    mitta = sana.length();
 
-    cout << "Syotetty sana on: " << sana << endl;
+    cout << "Sanassa on " << mitta << " kirjainta." << endl;
+
+    while (sana != arvattusana and
+            virheet < sallitut_virheet){
+        tulosta_riveja(1);
+        cout << "voit erehtyä vielä " << sallitut_virheet - virheet
+             << " kertaa" << endl;
+        tulosta_riveja(1);
+
+        cout << arvattusana << endl;
+        virheet ++;
+    }
+    tulosta_riveja(2);
+    if ( sana == arvattusana ) {
+        cout << "Onneksi olkoon, arvasit sanan oikein!" << endl;
+    } else {
+            cout << "Voi harmin paikka, arvaukset loppuivat kesken!" << endl;
+    }
 
 }
